f0d20fceddda001f77d2881350ef6c2c6cae1ede
Juanba <bauti777@gmail.com>
2023-07-05 15:27:14 -0300

    Base for new maps, area fix

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# Extra quest
-
-some random dungeon
+# CCPresetRevival
+This mod re-enables the Save Preset menu, which allows you to start the game on a specific part.
--- js/impact/feature/bgm/bgm-jsh.js
+++ js/impact/feature/bgm/bgm-jsh.js
@@ -1,10 +1,24 @@
 ig.module("impact.feature.bgm.bgm-jsh").requires("impact.feature.bgm.bgm").defines(function() {
 
     ig.merge(ig.BGM_TRACK_LIST, {
-        JShock: {
-            "path": "media/bgm/muJShock.ogg",
-            "loopEnd": 180.000,
+        clockTower: {
+            "path": "media/bgm/muClock.ogg",
+            "loopEnd": 195.602,
             "volume": 2.5
+        },
+		jShock: {
+            "path": "media/bgm/muJShock.ogg",
+            "loopEnd": 195.602,
+            "volume": 2.5
+        }
+    });
+
+    ig.merge(ig.BGM_DEFAULT_TRACKS, {
+        clockTower: {
+            field: {
+                track: "clockTower",
+                volume: 1
+            }
         }
     });
 });
