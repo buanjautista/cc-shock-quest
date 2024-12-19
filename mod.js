68ac5e8934f0e0289116aba72f5365cc0919a175
Juanba <bauti777@gmail.com>
2024-12-19 10:25:48 -0300

    version change, add combo weapon dependency

--- mod.js
+++ mod.js
@@ -1,4 +1,4 @@
 import "./js/impact/feature/bgm/bgm-jsh.js";
 import "./js/game/feature/weather/new-weather.js";
 import "./js/game/feature/map-content/clock-style.js";
-import "./js/combo-equip.js"
+// import "./js/combo-equip.js"
